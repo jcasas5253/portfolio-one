637329294dc5572a796a0a582302691ee8626e13
jcasas5253 <43353267+jcasas5253@users.noreply.github.com>
2019-07-25 15:51:10 -0700

    all scrolling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,16 +1,45 @@
-$("#animate-btn").click(function () {
+$("#animate-btn").click(function (e) {
+    e.preventDefault();
     $('html, body').animate({
-        scrollTop: $("#about-me").offset().top        
-    }, 2000);
+        scrollTop: 943,
+    }, 1000);
 });
 
-$("#nav-home").click(function () {
+$("#nav-about").click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: 943,
+    }, 1000);
+});
+
+$("#nav-home").click(function (e) {
+    e.preventDefault();
     $('html, body').animate({
         scrollTop: $(".main-container").offset().top
-    }, 2000);
+    }, 1000);
+});
+
+$(".up-btn").click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $(".main-container").offset().top
+    }, 1000);
 });
 
 
+$("#nav-portfolio").click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#portfolio").offset().top
+    }, 1000);
+});
+
+$("#nav-contact").click(function (e) {
+    e.preventDefault();
+    $('html, body').animate({
+        scrollTop: $("#contact").offset().top
+    }, 1000);
+});
 
 var stickyTop = $('.navbar').offset().top;
 $(window).on( 'scroll', function(){
