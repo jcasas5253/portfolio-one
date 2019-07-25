80ff7930341b63da3f3931524c803e18794d88aa
jcasas5253 <skedabble@gmail.com>
2019-07-24 20:57:19 -0700

    about me

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,24 +1,11 @@
 function smoothScroll() {
     window.scroll({
-        top: 2500,
+        top: 740,
         left: 0,
         behavior: 'smooth'
     });
 
 }
-
-
-//  var myNavigation = $('.navbar');
-//  stickyDiv = "sticky";
-//  myHeader = $('.main-container').height();
-
-//  $(window).scroll(function () {
-//     if ($(this).scrollTop() > myHeader) {
-//         myNavigation.addClass(stickyDiv);
-//     } else {
-//         myNavigation.removeClass(stickyDiv);
-//     }
-// });
 
 var stickyTop = $('.navbar').offset().top;
 $(window).on( 'scroll', function(){
