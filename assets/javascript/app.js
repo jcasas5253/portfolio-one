52f2b858ebf1ff8b5ba56125ae0d473237032066
jcasas5253 <43353267+jcasas5253@users.noreply.github.com>
2019-07-26 10:23:14 -0700

    fixed div height issue

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -42,11 +42,50 @@ $(".contact-nav").click(function (e) {
     }, 1000);
 });
 
-var stickyTop = $('.navbar').offset().top;
-$(window).on( 'scroll', function(){
-   if ($(window).scrollTop() >= stickyTop) {
-       $('.navbar').css({position: "fixed", top: "0px", right:"0px"});
-   } else {
-       $('.navbar').css({position: "relative", top: "0px"});
-   }
+//var stickyTop = $('.navbar').offset().top;
+//$(window).on( 'scroll', function(){
+//   if ($(window).scrollTop() >= stickyTop) {
+//       $('.navbar').css({position: "fixed", top: "0px", right:"0px"});
+//   } else {
+//       $('.navbar').css({position: "relative", top: "0px"});
+//   }
+//});
+
+function isScrolledIntoView(elem) {
+    var docViewTop = $(window).scrollTop();
+    var docViewBottom = docViewTop + $(window).height();
+    var elemTop = $(elem).offset().top;
+    var elemBottom = elemTop + $(elem).height();
+    return ((elemBottom >= docViewTop) && (elemTop <= docViewBottom) && (elemBottom <= docViewBottom) && (elemTop >= docViewTop));
+}
+
+$(window).scroll(function () {
+    if (isScrolledIntoView($('#about-me'))) {
+        $('.navbar').css("display", "flex");
+        $('#nav-about').css("border-bottom", "2px solid #4aaaa5");
+        $('#nav-portfolio').css("border-bottom", "none");
+        $('#nav-contact').css("border-bottom", "none");
+    }
+});
+
+$(window).scroll(function () {
+    if (isScrolledIntoView($('#portfolio'))) {
+        $('#nav-about').css("border-bottom", "none");
+        $('#nav-contact').css("border-bottom", "none");
+        $('#nav-portfolio').css("border-bottom", "2px solid #4aaaa5");
+    }
+});
+
+$(window).scroll(function () {
+    if (isScrolledIntoView($('#contact'))) {
+        $('#nav-about').css("border-bottom", "none");
+        $('#nav-portfolio').css("border-bottom", "none");
+        $('#nav-contact').css("border-bottom", "2px solid #4aaaa5");
+    }
+});
+
+$(window).scroll(function () {
+    if (isScrolledIntoView($('.invisible'))) {
+        $('.navbar').css("display", "none");
+    }
 });
